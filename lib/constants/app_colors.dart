f88f451844e4132c520e51e2693c4614c7d8c8b3
Bahaa Assaf <bahaa7assaf@gmail.com>
2025-10-13 01:19:06 +0300

    تهيئة بيئة العمل واضافة مجلدات الصور والبدء بالكود الاول لصفحة splach_screen بشكل اولي

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkGreen = Color(0xFF002B23);
+  static const Color gold = Color(0xFFFFD700);
+}
---
+++ lib/constants/app_text_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static TextStyle republicText = const TextStyle(
+    color: Color.fromARGB(255, 255, 255, 255),
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Amiri',
+  );
+
+  static TextStyle titleText = const TextStyle(
+    color: Color.fromARGB(255, 255, 255, 255),
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Tajawal',
+  );
+}
